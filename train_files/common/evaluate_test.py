af9757f1267f51122fadb19b6e68ade15a9b0ca9
galik_a <74008165+agalikeev@users.noreply.github.com>
2023-11-07 22:25:36 +0300

    Update evaluate_test.py

--- train_files/common/evaluate_test.py
+++ train_files/common/evaluate_test.py
@@ -32,8 +32,8 @@ from dual import (
 from environments import Branching as Environment  # environments
 from rewards import TimeLimitDualIntegral as BoundIntegral  # rewards
 
-inst = "/content/explore_nuri/Nuri/instances/0train/mas76.mps.gz"
-print(inst)
+inst = "/content/explore_nuri/Nuri/instances/train_test/mas76.mps.gz"
+print(f"instance:{inst}")
 
 time_limit = 15 * 60
 
@@ -46,41 +46,56 @@ observation, action_set, reward, done, info = env.reset(inst)
 correct_predictions = 0
 total_predictions = 0
 rand_accuracy = 0
+
+sum_metric1 = 0 # policy_num / strbr_num
+sum_metric2 = 0 # policy_score / strbr_score
+sum_rand_metric1 = 0
+sum_rand_metric2 = 0
+
 total_action_set = 0
 sum_acc = 0
-while not done:
-    policy_action = policy(action_set, observation)
 
+while not done:
+
+    policy_action = policy(action_set, observation)
     strbr_scores = strbr.extract(env.model, done)
     strbr_action = action_set[strbr_scores[action_set].argmax()]
 
-    #if torch.allclose(policy_action, strbr_action):
     if policy_action.item() == strbr_action:
         correct_predictions += 1
     total_predictions += 1
     total_action_set += len(action_set)
     rand_accuracy =  total_predictions / total_action_set
+
     policty_action_id = np.where(action_set == policy_action.item())[0][0]
     policy_score = strbr_scores[action_set][policty_action_id]
     sorted_strbr_scores = sorted(strbr_scores[action_set])
     policy_score_top = np.where(sorted_strbr_scores == policy_score)[0][0]
+    
+    sum_metric1 += policy_score_top / len(action_set)
+    sum_metric2 += policy_score / sorted_strbr_scores[-1]
+
+    #sum_rand_metric1 += 
+    sum_rand_metric2 += sum(sorted_strbr_scores) / (len(sorted_strbr_scores) * sorted_strbr_scores[-1])
+
     sum_acc = sum_acc + policy_score_top / len(action_set)
     print("======================================")
     print(f"iteration: {total_predictions}")
     #print(f"strbr_scores[action_set]: {strbr_scores[action_set]}")
     print(f"sorted sb: {sorted_strbr_scores}")
     print(f"policy_score: {policy_score}")
-    #print(f"len(sorted_strbr_scores):{len(sorted_strbr_scores)}, len(action_set): {len(action_set)}, len(strbr_scores[action_set]): {len(strbr_scores[action_set])}")
-    #print(action_set, type(action_set), policty_action_id)
-    #print(f"policy_score_top: {policy_score_top}, len(action): {len(action_set)}")
+    print(f"policy_score_top {policy_score_top}")
 
     print(f"policy_action: {policy_action}")
     print(f"strbr_action: {strbr_action}")
-    #print(f"action_set: {action_set}")
-    #print(f"strbr_scores: {strbr_scores[action_set]}")
+    print(f"action_set: {action_set}")
+
     print(f"current accuracy: {correct_predictions/total_predictions}")
     print(f"random accuracy:  {rand_accuracy}")
-    print(f"top_acc: {sum_acc/total_predictions}")
+
+    print(f"metric1: {sum_metric1 / total_predictions}")
+    print(f"metric2: {sum_metric2 / total_predictions}")
+    print(f"rand_metric2: {sum_rand_metric2 / total_predictions}")
 
     observation, action_set, reward, done, info = env.step(strbr_action)
 
